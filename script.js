5968fc0854926f5a2938da5d3573429bd07b8094
Jackson tech <jacksonlumumba275@gmail.com>
2026-04-25 00:08:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,5 @@ function scrollToProjects() {
 }
 
 function contactMe() {
-    alert("Email me at: jacksonlumumba275@gmail.com");
-}
-
-function openProject() {
-    alert("Project link coming soon!");
-}
-
-// Scroll animation
-window.addEventListener("scroll", () => {
-    document.querySelectorAll(".project-card").forEach(card => {
-        const top = card.getBoundingClientRect().top;
-
-        if (top < window.innerHeight - 100) {
-            card.classList.add("show");
-        }
-    });
-});
-
-// Dark mode
-function toggleMode() {
-    document.body.classList.toggle("light-mode");
+    window.location.href = "mailto:jacksonlumumba275@gmail.com";
 }
